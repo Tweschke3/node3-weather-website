ffc94f48dacc64aeb739b7a9ac0f23afa6cc7549
Lennart <lennart@buesge.de>
2020-08-10 15:48:11 +0200

    ! added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,7 @@ const forecast = (address, callback) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
-            callback('Unable to find location', undefined)
+            callback('Unable to find location!', undefined)
         } else {
             callback(undefined, body)
         }
